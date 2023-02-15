5970a081f305d56fc94f41482ee9e9dc30b7e50b
vanesa pallares <vpallaressolis@gmail.com>
2023-02-15 15:28:21 -0500

    feat(Hosting):add tema de hosting

---
+++ src/hosting/index.js
@@ -0,0 +1,12 @@
+
+
+// console.log(nameOfDog); //undefined
+// var nameOfDog='Elmo';
+
+
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`);
+}
+var elmo='elmito';
